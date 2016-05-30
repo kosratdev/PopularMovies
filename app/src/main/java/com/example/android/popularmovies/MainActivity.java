4df63639d291099ca23b15a10b1414adfb5c35ba
Kosrat D. Ahmad <kosrat.d.ahmad@gmail.com>
2016-05-30 23:40:12 +0300

    feat: add movies fragment

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -9,5 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new MoviesFragment())
+                    .commit();
+        }
     }
 }
---
+++ app/src/main/java/com/example/android/popularmovies/MoviesFragment.java
@@ -0,0 +1,24 @@
+package com.example.android.popularmovies;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Encapsulates fetching the movies and displaying it as a {@link GridView} layout.
+ * Created by kosrat on 5/30/16.
+ */
+public class MoviesFragment extends Fragment {
+
+    public MoviesFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
