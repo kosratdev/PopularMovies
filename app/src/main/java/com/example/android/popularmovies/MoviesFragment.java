aa4c99d9960d65cd58e95c510296117618572fcc
Kosrat D. Ahmad <kosrat.d.ahmad@gmail.com>
2016-06-02 19:12:18 +0300

    feat: getting data from internet

--- app/src/main/java/com/example/android/popularmovies/MoviesFragment.java
+++ app/src/main/java/com/example/android/popularmovies/MoviesFragment.java
@@ -1,8 +1,11 @@
 package com.example.android.popularmovies;
 
 import android.content.Context;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +14,13 @@ import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Encapsulates fetching the movies and displaying it as a {@link GridView} layout.
@@ -36,9 +46,21 @@ public class MoviesFragment extends Fragment {
                         Toast.LENGTH_SHORT).show();
             }
         });
+        updateMovies();
         return rootView;
     }
 
+    /**
+     * update movie posters by getting data from themoviedb API
+     */
+    private void updateMovies(){
+        FetchMoviesTask moviesTask = new FetchMoviesTask();
+        moviesTask.execute();
+    }
+
+    /**
+     * Grid view image adapter
+     */
     public class ImageAdapter extends BaseAdapter {
         private Context mContext;
 
@@ -85,5 +107,96 @@ public class MoviesFragment extends Fragment {
                 R.mipmap.ic_launcher, R.mipmap.ic_launcher,
                 R.mipmap.ic_launcher, R.mipmap.ic_launcher,
         };
+    }
+
+    /**
+     * getting movie data from themoviedb API by creating a new thread to work in background.
+     */
+    public class FetchMoviesTask extends AsyncTask<Void, Void, String[]>{
+
+        private final String LOG_TAG = FetchMoviesTask.class.getSimpleName();
+
+        @Override
+        protected String[] doInBackground(Void... params) {
+
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String movieJsonStr = null;
+
+            String sortType="popular";
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at http://themoviedb.org/
+                final String MOVIE_BASE_URL ="https://api.themoviedb.org/3/movie/"+sortType;
+
+                final String API_PARAM = "api_key";
+
+                Uri builtUri = Uri.parse(MOVIE_BASE_URL).buildUpon()
+                        .appendQueryParameter(API_PARAM, BuildConfig.THE_MOVIE_DB_API_KEY)
+                        .build();
+                Log.i(LOG_TAG, builtUri.toString());
+
+                URL url = new URL(builtUri.toString());
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                movieJsonStr = buffer.toString();
+                Log.i(LOG_TAG,movieJsonStr);
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+//            try {
+//                //return getMovieDataFromJson(movieJsonStr);
+//            } catch (JSONException e) {
+//                Log.e(LOG_TAG, e.getMessage(), e);
+//                e.printStackTrace();
+//            }
+
+            // This will only happen if there was an error getting or parsing the forecast.
+            return null;
+        }
     }
 }
