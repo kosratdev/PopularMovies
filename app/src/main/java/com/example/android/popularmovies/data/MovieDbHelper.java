1c5006e15f8cab3b792282c1847d7ac5fecc3698
Kosrat D. Ahmad <kosrat.d.ahmad@gmail.com>
2016-06-09 23:22:11 +0300

    feat: add MovieContract and MovieDbHelper class for store data

---
+++ app/src/main/java/com/example/android/popularmovies/data/MovieDbHelper.java
@@ -0,0 +1,51 @@
+package com.example.android.popularmovies.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.android.popularmovies.data.MovieContract.MovieEntry;
+
+/**
+ * Created by kosrat on 6/9/16.
+ */
+public class MovieDbHelper extends SQLiteOpenHelper {
+
+    // if you change the database schema, you must increment the database version
+    private static final int DATABSE_VERSION = 1;
+
+    static final String DATABASE_NAME = "move.db";
+
+    public MovieDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABSE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        // create sql query to create the movie table
+        final String SQL_CREATE_MOVIE_TABLE = "CREATE TABLE " + MovieEntry.TABLE_NAME + " (" +
+                MovieEntry._ID + " INTEGER PRIMARY KEY, "+
+                MovieEntry.COLUMN_MOIVE_ID + " INTEGER NOT NULL, "+
+                MovieEntry.COLUMN_MOVIE_TITLE + " TEXT NOT NULL, " +
+                MovieEntry.COLUMN_MOVIE_POSTER + " TEXT NOT NULL, " +
+                MovieEntry.COLUMN_MOVIE_OVERVIEW + " TEXT NOT NULL, " +
+                MovieEntry.COLUMN_MOVIE_RATED + " REAL NOT NULL, " +
+                MovieEntry.COLUMN_MOVIE_RELEASE + " TEXT NOT NULL " +
+                ");";
+
+        db.execSQL(SQL_CREATE_MOVIE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        // Note that this only fires if you change the version number for your database.
+        // It does NOT depend on the version number for your application.
+        // If you want to update the schema without wiping data, commenting out the next 2 lines
+        // should be your top priority before modifying this method.
+        db.execSQL("DROP TABLE IF EXISTS " + MovieEntry.TABLE_NAME);
+        onCreate(db);
+    }
+}
