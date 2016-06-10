994274090ae719160eced2621efc0ca29c551577
Kosrat D. Ahmad <kosrat.d.ahmad@gmail.com>
2016-06-10 14:21:58 +0300

    feat: save and restore data on rotate

--- app/src/main/java/com/example/android/popularmovies/MoviesFragment.java
+++ app/src/main/java/com/example/android/popularmovies/MoviesFragment.java
@@ -1,17 +1,17 @@
 /**
  * Copyright 2016 Kosrat D. Ahmed
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * <p/>
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.example.android.popularmovies;
 
@@ -22,6 +22,7 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -57,30 +58,61 @@ public class MoviesFragment extends Fragment {
 
     private GridViewAdapter mMovieAdapter;
     ArrayList<Movie> mMovieList = new ArrayList<>();
+    private GridView mGridView;
 
+    private final String MOVIE_DATA = "movie_data";
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        if (savedInstanceState != null) {
+            // restore saved movie data
+            mMovieList = savedInstanceState.getParcelableArrayList(MOVIE_DATA);
+            setGridView(mMovieList);
+        } else {
+            // fetch data
+            updateMovies();
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        // save movie data on rotate
+        outState.putParcelableArrayList(MOVIE_DATA, mMovieList);
+        super.onSaveInstanceState(outState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        mMovieAdapter = new GridViewAdapter(getActivity(), mMovieList);
 
-        GridView gridview = (GridView) rootView.findViewById(R.id.gridview);
-        gridview.setAdapter(mMovieAdapter);
+        mGridView = (GridView) rootView.findViewById(R.id.gridview);
+        setGridView(mMovieList);
 
-        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
                 Movie movie = mMovieAdapter.getItem(position);
-                Intent intent= new Intent(getActivity(), DetailActivity.class)
+                Intent intent = new Intent(getActivity(), DetailActivity.class)
                         .putExtra("movieDetail", movie);
                 startActivity(intent);
             }
         });
 
         return rootView;
+    }
+
+    /**
+     * Set gridview with movie's thumbnail
+     * @param movieList is a movie list thumbnail
+     */
+    private void setGridView(ArrayList<Movie> movieList) {
+        mMovieAdapter = new GridViewAdapter(getActivity(), movieList);
+        mGridView.setAdapter(mMovieAdapter);
     }
 
     /**
@@ -94,7 +126,7 @@ public class MoviesFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        updateMovies();
+        //updateMovies();
     }
 
     /**
