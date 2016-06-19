2b3196f59aa10bbeef77082061f9c623bbe1c26b
Kosrat D. Ahmad <kosrat.d.ahmad@gmail.com>
2016-06-20 01:19:48 +0300

    feat: add content provider for movie db

--- app/src/main/java/com/example/android/popularmovies/data/MovieContract.java
+++ app/src/main/java/com/example/android/popularmovies/data/MovieContract.java
@@ -15,14 +15,43 @@
  */
 package com.example.android.popularmovies.data;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
  * Created by kosrat on 6/9/16.
+ *
+ * Defines table and column names for the movie database.
+ *
  */
 public class MovieContract {
 
+    // The "Content authority" is a name for the entire content provider, similar to the
+    // relationship between a domain name and its website.  A convenient string to use for the
+    // content authority is the package name for the app, which is guaranteed to be unique on the
+    // device.
+    public static final String CONTENT_AUTHORITY = "com.example.android.popularmovies";
+
+    // Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+    // the content provider.
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    // Possible paths (appended to base content URI for possible URI's)
+    // For instance, content://com.example.android.popularmovies/movies/ is a valid path for
+    // looking at movies data. content://com.example.android.popularmovies/givemeroot/ will fail,
+    // as the ContentProvider hasn't been given any information on what to do with "givemeroot".
+    // At least, let's hope not.  Don't be that dev, reader.  Don't be that dev.
+    public static final String PATH_MOVIE = "movies";
+
+    /* Inner class that defines the table contents of the location table */
     public static class MovieEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIE).build();
+
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" +
+                CONTENT_AUTHORITY + "/" + PATH_MOVIE;
 
         public static final String TABLE_NAME = "movies";
 
@@ -46,5 +75,32 @@ public class MovieContract {
 
         // column for storing backdrop image of the movie
         public static final String COLUMN_MOVIE_BACKDROP = "backdrop_path";
+
+        public static Uri buildMovieUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+        // Store all column of movies table we will use it for the Detail view.
+        // Specify the columns we need.
+        public static final String[] MOVIE_COLUMNS={
+                COLUMN_MOIVE_ID,
+                COLUMN_MOVIE_TITLE,
+                COLUMN_MOVIE_POSTER,
+                COLUMN_MOVIE_OVERVIEW,
+                COLUMN_MOVIE_RATED,
+                COLUMN_MOVIE_RELEASE,
+                COLUMN_MOVIE_BACKDROP
+        };
+
+        // These indices are tied to MOVIE_COLUMNS.  If MOVIE_COLUMNS changes, these
+        // must change.
+        public static final int COL_MOIVE_ID = 0;
+        public static final int COL_MOIVE_TITLE = 1;
+        public static final int COL_MOIVE_POSTER = 2;
+        public static final int COL_MOIVE_OVERVIEW = 3;
+        public static final int COL_MOIVE_RATED = 4;
+        public static final int COL_MOIVE_RELEASE = 5;
+        public static final int COL_MOIVE_BACKDROP = 6;
+
     }
 }
---
+++ app/src/main/java/com/example/android/popularmovies/data/MovieProvider.java
@@ -0,0 +1,157 @@
+package com.example.android.popularmovies.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by kosrat on 6/20/16.
+ */
+public class MovieProvider extends ContentProvider {
+
+    static final int MOVIES = 100;
+    private MovieDbHelper mOpenHelper;
+
+    public static final UriMatcher sUriMacher = buildUriMatcher();
+
+    /**
+     * This UriMatcher will match each URI to the MOVIE
+     *
+     * @return UriMatcher
+     */
+    private static UriMatcher buildUriMatcher() {
+
+        // All paths added to the UriMatcher have a corresponding code to return when a match is
+        // found.  The code passed into the constructor represents the code to return for the root
+        // URI.  It's common to use NO_MATCH as the code for this case.
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = MovieContract.CONTENT_AUTHORITY;
+
+        // For each type of URI you want to add, create a corresponding code.
+        matcher.addURI(authority, MovieContract.PATH_MOVIE, MOVIES);
+
+        return matcher;
+    }
+
+    @Override
+    public boolean onCreate() {
+        mOpenHelper = new MovieDbHelper(getContext());
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+
+        Cursor cursor;
+        switch (sUriMacher.match(uri)){
+            case MOVIES:
+                cursor = mOpenHelper.getReadableDatabase().query(
+                        MovieContract.MovieEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder
+                );
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: "+ uri);
+        }
+
+        if(getContext() != null){
+            cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        }
+        return cursor;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        final int match = sUriMacher.match(uri);
+        switch (match) {
+            case MOVIES:
+                return MovieContract.MovieEntry.CONTENT_TYPE;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final int match = sUriMacher.match(uri);
+
+        Uri returnedUri;
+
+        switch (match) {
+            case MOVIES:
+                long _id = db.insert(MovieContract.MovieEntry.TABLE_NAME, null, values);
+                if (_id > 0) {
+                    returnedUri = MovieContract.MovieEntry.buildMovieUri(_id);
+                } else {
+                    throw new SQLException("Failed to insert row into " + uri);
+                }
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        if (getContext() != null) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return returnedUri;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final int match = sUriMacher.match(uri);
+
+        int rowDeleted;
+
+        // this makes delete all rows return the number of rows deleted.
+        if (null == selection) selection = "1";
+
+        switch (match) {
+            case MOVIES:
+                rowDeleted = db.delete(MovieContract.MovieEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        if (rowDeleted != 0 && getContext() != null) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return rowDeleted;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final int match = sUriMacher.match(uri);
+
+        int rowUpdated;
+
+        switch (match) {
+            case MOVIES:
+                rowUpdated = db.update(MovieContract.MovieEntry.TABLE_NAME, values, selection, selectionArgs);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        if (rowUpdated != 0 && getContext() != null) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return rowUpdated;
+    }
+}
